1ab5284408a099eef681ff68ec61c9ac21f94b7e
Chhokmah0 <haoyimin1214@foxmail.com>
2024-08-10 17:36:43 +0800

    feat: 实现了 Drop 特性
    实现了 Drop 特性，现在当链表中仍有元素时，依然可以安全释放。

--- src/safe_linked_list.rs
+++ src/safe_linked_list.rs
@@ -5,6 +5,10 @@ pub struct SafeLinkedList<'id, T> {
     head: Option<Half<'id, T>>,
     tail: Option<Half<'id, T>>,
     len: usize,
+    // 为了可以安全 drop，token 必须放在结构体内
+    // 这导致一个 token 内只能创建一个 LinkedList 结构
+    // 不过如果给 token 加上读写锁，或许可以比较方便地多线程？
+    token: GhostToken<'id>,
 }
 
 struct Node<'id, T> {
@@ -29,11 +33,12 @@ impl<'id, T> Node<'id, T> {
 }
 
 impl<'id, T> SafeLinkedList<'id, T> {
-    pub fn new() -> Self {
+    pub fn with_token(token: GhostToken<'id>) -> Self {
         Self {
             head: None,
             tail: None,
             len: 0,
+            token,
         }
     }
 
@@ -41,11 +46,11 @@ impl<'id, T> SafeLinkedList<'id, T> {
         self.len == 0
     }
 
-    pub fn push_back(&mut self, elem: T, token: &mut GhostToken<'id>) {
+    pub fn push_back(&mut self, elem: T) {
         let (one, two) = Full::split::<1, 1>(Full::new(GhostCell::new(Node::with_elem(elem))));
         if let Some(left) = self.tail.take() {
-            left.borrow_mut(token).next = Some(one);
-            two.borrow_mut(token).prev = Some(left);
+            left.borrow_mut(&mut self.token).next = Some(one);
+            two.borrow_mut(&mut self.token).prev = Some(left);
             self.tail = Some(two);
         } else {
             self.head = Some(one);
@@ -54,10 +59,10 @@ impl<'id, T> SafeLinkedList<'id, T> {
         self.len += 1;
     }
 
-    pub fn pop_back(&mut self, token: &mut GhostToken<'id>) -> Option<T> {
+    pub fn pop_back(&mut self) -> Option<T> {
         let two = self.tail.take()?;
-        let one = if let Some(left) = two.borrow_mut(token).prev.take() {
-            let one = left.borrow_mut(token).next.take().unwrap();
+        let one = if let Some(left) = two.borrow_mut(&mut self.token).prev.take() {
+            let one = left.borrow_mut(&mut self.token).next.take().unwrap();
             self.tail = Some(left);
             one
         } else {
@@ -68,11 +73,11 @@ impl<'id, T> SafeLinkedList<'id, T> {
         Some(Full::into_inner(Full::join(one, two)).into_inner().elem)
     }
 
-    pub fn push_front(&mut self, elem: T, token: &mut GhostToken<'id>) {
+    pub fn push_front(&mut self, elem: T) {
         let (one, two) = Full::split::<1, 1>(Full::new(GhostCell::new(Node::with_elem(elem))));
         if let Some(right) = self.head.take() {
-            right.borrow_mut(token).prev = Some(one);
-            two.borrow_mut(token).next = Some(right);
+            right.borrow_mut(&mut self.token).prev = Some(one);
+            two.borrow_mut(&mut self.token).next = Some(right);
             self.head = Some(two);
         } else {
             self.head = Some(one);
@@ -81,10 +86,10 @@ impl<'id, T> SafeLinkedList<'id, T> {
         self.len += 1;
     }
 
-    pub fn pop_front(&mut self, token: &mut GhostToken<'id>) -> Option<T> {
+    pub fn pop_front(&mut self) -> Option<T> {
         let two = self.head.take()?;
-        let one = if let Some(right) = two.borrow_mut(token).next.take() {
-            let one = right.borrow_mut(token).prev.take().unwrap();
+        let one = if let Some(right) = two.borrow_mut(&mut self.token).next.take() {
+            let one = right.borrow_mut(&mut self.token).prev.take().unwrap();
             self.head = Some(right);
             one
         } else {
@@ -96,27 +101,31 @@ impl<'id, T> SafeLinkedList<'id, T> {
     }
 }
 
-// IntoIter 存在的期间会占用整个 token，这是否合理呢？
-// 应该是合理的，当有多个不相关数据结构时，应该创建多个 token 分别进行管理
-pub struct IntoIter<'a, 'id, T> {
-    token: &'a mut GhostToken<'id>,
-    list: SafeLinkedList<'id, T>,
-}
-
-impl<'id, T> SafeLinkedList<'id, T> {
-    pub fn into_iter<'a>(self, token: &'a mut GhostToken<'id>) -> IntoIter<'a, 'id, T> {
-        IntoIter {
-            token: token,
-            list: self,
+impl<'id, T> Drop for SafeLinkedList<'id, T> {
+    fn drop(&mut self) {
+        while !self.is_empty() {
+            self.pop_front();
         }
     }
 }
 
-impl<'a, 'id, T> Iterator for IntoIter<'a, 'id, T> {
+// IntoIter 存在的期间会占用整个 token，这是否合理呢？
+// 应该是合理的，当有多个不相关数据结构时，应该创建多个 token 分别进行管理
+pub struct IntoIter<'id, T> {
+    list: SafeLinkedList<'id, T>,
+}
+
+impl<'id, T> SafeLinkedList<'id, T> {
+    pub fn into_iter(self) -> IntoIter<'id, T> {
+        IntoIter { list: self }
+    }
+}
+
+impl<'id, T> Iterator for IntoIter<'id, T> {
     type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.list.pop_front(self.token)
+        self.list.pop_front()
     }
 }
 
@@ -126,9 +135,9 @@ pub struct Iter<'a, 'id, T> {
 }
 
 impl<'id, T> SafeLinkedList<'id, T> {
-    pub fn iter<'a, 'b: 'a>(&'a self, token: &'b GhostToken<'id>) -> Iter<'a, 'id, T> {
+    pub fn iter<'a>(&'a self) -> Iter<'a, 'id, T> {
         Iter {
-            token,
+            token: &self.token,
             node_ptr: self.head.as_ref(),
         }
     }
@@ -153,98 +162,98 @@ mod tests {
 
     #[test]
     fn push_and_pop_back() {
-        GhostToken::new(|mut token| {
-            let mut list = SafeLinkedList::new();
-            assert_eq!(None, list.pop_back(&mut token));
+        GhostToken::new(|token| {
+            let mut list = SafeLinkedList::with_token(token);
+            assert_eq!(None, list.pop_back());
 
-            list.push_back(1, &mut token);
+            list.push_back(1);
 
-            list.push_back(2, &mut token);
-            list.push_back(3, &mut token);
-            assert_eq!(Some(3), list.pop_back(&mut token));
-            assert_eq!(Some(2), list.pop_back(&mut token));
+            list.push_back(2);
+            list.push_back(3);
+            assert_eq!(Some(3), list.pop_back());
+            assert_eq!(Some(2), list.pop_back());
 
-            list.push_back(3, &mut token);
-            assert_eq!(Some(3), list.pop_back(&mut token));
+            list.push_back(3);
+            assert_eq!(Some(3), list.pop_back());
 
-            assert_eq!(Some(1), list.pop_back(&mut token));
-            assert_eq!(None, list.pop_back(&mut token));
+            assert_eq!(Some(1), list.pop_back());
+            assert_eq!(None, list.pop_back());
         });
     }
 
     #[test]
     fn push_and_pop_front() {
-        GhostToken::new(|mut token| {
-            let mut list = SafeLinkedList::new();
-            assert_eq!(None, list.pop_front(&mut token));
+        GhostToken::new(|token| {
+            let mut list = SafeLinkedList::with_token(token);
+            assert_eq!(None, list.pop_front());
 
-            list.push_front(1, &mut token);
+            list.push_front(1);
 
-            list.push_front(2, &mut token);
-            list.push_front(3, &mut token);
-            assert_eq!(Some(3), list.pop_front(&mut token));
-            assert_eq!(Some(2), list.pop_front(&mut token));
+            list.push_front(2);
+            list.push_front(3);
+            assert_eq!(Some(3), list.pop_front());
+            assert_eq!(Some(2), list.pop_front());
 
-            list.push_front(3, &mut token);
-            assert_eq!(Some(3), list.pop_front(&mut token));
+            list.push_front(3);
+            assert_eq!(Some(3), list.pop_front());
 
-            assert_eq!(Some(1), list.pop_front(&mut token));
-            assert_eq!(None, list.pop_front(&mut token));
+            assert_eq!(Some(1), list.pop_front());
+            assert_eq!(None, list.pop_front());
         });
     }
 
     #[test]
     fn front_and_back() {
-        GhostToken::new(|mut token| {
-            let mut list = SafeLinkedList::new();
-            assert_eq!(None, list.pop_front(&mut token));
+        GhostToken::new(|token| {
+            let mut list = SafeLinkedList::with_token(token);
+            assert_eq!(None, list.pop_front());
 
-            list.push_front(1, &mut token);
-            list.push_front(2, &mut token);
-            list.push_front(3, &mut token);
+            list.push_front(1);
+            list.push_front(2);
+            list.push_front(3);
 
-            assert_eq!(Some(1), list.pop_back(&mut token));
-            assert_eq!(Some(2), list.pop_back(&mut token));
-            assert_eq!(Some(3), list.pop_back(&mut token));
-            assert_eq!(None, list.pop_back(&mut token));
+            assert_eq!(Some(1), list.pop_back());
+            assert_eq!(Some(2), list.pop_back());
+            assert_eq!(Some(3), list.pop_back());
+            assert_eq!(None, list.pop_back());
 
-            list.push_back(1, &mut token);
-            list.push_back(2, &mut token);
-            list.push_back(3, &mut token);
+            list.push_back(1);
+            list.push_back(2);
+            list.push_back(3);
 
-            assert_eq!(Some(1), list.pop_front(&mut token));
-            assert_eq!(Some(2), list.pop_front(&mut token));
-            assert_eq!(Some(3), list.pop_front(&mut token));
-            assert_eq!(None, list.pop_front(&mut token));
+            assert_eq!(Some(1), list.pop_front());
+            assert_eq!(Some(2), list.pop_front());
+            assert_eq!(Some(3), list.pop_front());
+            assert_eq!(None, list.pop_front());
         });
     }
 
     #[test]
     fn into_iter() {
-        GhostToken::new(|mut token| {
-            let mut list = SafeLinkedList::new();
-            list.push_back(1, &mut token);
-            list.push_back(2, &mut token);
-            list.push_back(3, &mut token);
-            let vec: Vec<_> = list.into_iter(&mut token).collect();
+        GhostToken::new(|token| {
+            let mut list = SafeLinkedList::with_token(token);
+            list.push_back(1);
+            list.push_back(2);
+            list.push_back(3);
+            let vec: Vec<_> = list.into_iter().collect();
             assert_eq!(vec, vec![1, 2, 3]);
         });
     }
 
     #[test]
     fn multi_lists() {
-        GhostToken::new(|mut token1| {
-            GhostToken::new(move |mut token2| {
-                let mut list1 = SafeLinkedList::new();
-                let mut list2 = SafeLinkedList::new();
-                list1.push_back(1, &mut token1);
-                list1.push_back(2, &mut token1);
-                list2.push_back(1, &mut token2);
-                list2.push_back(2, &mut token2);
-                for _ in list1.into_iter(&mut token1) {
-                    list2.push_back(3, &mut token2);
+        GhostToken::new(|token1| {
+            GhostToken::new(move |token2| {
+                let mut list1 = SafeLinkedList::with_token(token1);
+                let mut list2 = SafeLinkedList::with_token(token2);
+                list1.push_back(1);
+                list1.push_back(2);
+                list2.push_back(1);
+                list2.push_back(2);
+                for _ in list1.into_iter() {
+                    list2.push_back(3);
                 }
-                let vec: Vec<_> = list2.into_iter(&mut token2).collect();
+                let vec: Vec<_> = list2.into_iter().collect();
                 assert_eq!(vec, vec![1, 2, 3, 3]);
             })
         })
@@ -252,34 +261,44 @@ mod tests {
 
     #[test]
     fn iter() {
-        GhostToken::new(|mut token| {
-            let mut list = SafeLinkedList::new();
-            list.push_back(1, &mut token);
-            list.push_back(2, &mut token);
-            list.push_back(3, &mut token);
-            let vec: Vec<_> = list.iter(&token).map(|x| *x).collect();
-            let vec2: Vec<_> = list.into_iter(&mut token).collect();
+        GhostToken::new(|token| {
+            let mut list = SafeLinkedList::with_token(token);
+            list.push_back(1);
+            list.push_back(2);
+            list.push_back(3);
+            let vec: Vec<_> = list.iter().map(|x| *x).collect();
+            let vec2: Vec<_> = list.into_iter().collect();
             assert_eq!(vec, vec2);
         })
     }
 
     #[test]
     fn iter2() {
-        GhostToken::new(|mut token| {
-            let mut list = SafeLinkedList::new();
-            list.push_back(1, &mut token);
-            list.push_back(2, &mut token);
-            list.push_back(3, &mut token);
+        GhostToken::new(|token| {
+            let mut list = SafeLinkedList::with_token(token);
+            list.push_back(1);
+            list.push_back(2);
+            list.push_back(3);
             let mut cnt = 0;
-            for _ in list.iter(&token) {
-                for _ in list.iter(&token) {
+            for _ in list.iter() {
+                for _ in list.iter() {
                     cnt += 1;
                 }
             }
             assert_eq!(cnt, 9);
-            list.pop_back(&mut token);
-            list.pop_back(&mut token);
-            list.pop_back(&mut token);
+
+            let vec: Vec<_> = list.into_iter().collect();
+            assert_eq!(vec, vec![1, 2, 3]);
+        })
+    }
+
+    #[test]
+    fn drop() {
+        GhostToken::new(|token| {
+            let mut list = SafeLinkedList::with_token(token);
+            list.push_back(1);
+            list.push_back(2);
+            list.push_back(3);
         })
     }
 }
